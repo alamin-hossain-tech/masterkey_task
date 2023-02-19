c289c9dbc8f4d85aea0caafeb66b9d24a57c0619
alamin-merndev <alaminnishat@gmail.com>
2023-02-19 17:24:50 +0600

    product card finish

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,7 @@
 import { Input, Radio } from "@material-tailwind/react";
 import React from "react";
 import "./Home.css";
+import { HiHeart, HiMinus, HiPlus } from "react-icons/hi";
 
 const Home = () => {
   return (
@@ -29,9 +30,9 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="px-5">
+      <div className="px-5 flex gap-12">
         {/* Product Card  */}
-        <div className="flex p-5 shadow-md rounded-md ">
+        <div className="flex p-5 product_card w-3/4">
           <img
             src="https://i.ibb.co/vVLrfVd/g835-SLOHDG-F-1-removebg-preview-1.png"
             alt=""
@@ -66,29 +67,52 @@ const Home = () => {
               time. Apple Wi crip & Closs{" "}
             </p>
             <p className="text-[#07FFD2]">Price $ 450.55</p>
-            <div className="flex items-center gap-3">
-              <p className="pb-2">Color:</p>
-              <div>
-                {" "}
-                <input type="radio" name="color" id="green" value="green" />
-                <label for="green">
-                  <span class="green"></span>
-                </label>
-                <input type="radio" name="color" id="cyan" />
-                <label for="cyan">
-                  <span class="cyan"></span>
-                </label>
-                <input type="radio" name="color" id="blue" />
-                <label for="blue">
-                  <span class="blue"></span>
-                </label>
-                <input type="radio" name="color" id="violet" />
-                <label for="violet">
-                  <span class="violet"></span>
-                </label>
+            <div className="flex gap-8">
+              <div className="flex items-center gap-3">
+                <p className="pb-2">Color:</p>
+                <div>
+                  {" "}
+                  <input type="radio" name="color" id="green" value="green" />
+                  <label for="green">
+                    <span class="green"></span>
+                  </label>
+                  <input type="radio" name="color" id="cyan" />
+                  <label for="cyan">
+                    <span class="cyan"></span>
+                  </label>
+                  <input type="radio" name="color" id="blue" />
+                  <label for="blue">
+                    <span class="blue"></span>
+                  </label>
+                  <input type="radio" name="color" id="violet" />
+                  <label for="violet">
+                    <span class="violet"></span>
+                  </label>
+                </div>
+              </div>
+              <div className="flex items-center gap-3 quantity_box px-3">
+                <HiMinus className="inline qty-icon"></HiMinus>
+                <p>1</p>
+                <HiPlus className="inline qty-icon"></HiPlus>
               </div>
             </div>
+            <div className="flex items-center gap-5 pt-8">
+              <div>
+                <HiHeart className="inline text-[#38FF4C] text-3xl"></HiHeart>
+              </div>
+              <p className="py-2 px-1 border-2 border-[#38FF4C] rounded hover:cursor-pointer">
+                Add to Cart
+              </p>
+              <button className="px-5 py-2 font-semibold bg-[#38FF4C] rounded">
+                Buy Now
+              </button>
+            </div>
           </div>
+        </div>
+
+        <div className="text-white p-3 bg-[url('https://i.ibb.co/71JjTmC/Frame-22.png')] w-1/4  h-[212px] bg-contain bg-no-repeat flex flex-col justify-between p-3">
+          <h2>Summer headphones from top brands</h2>
+          <h4>Buy it Now</h4>
         </div>
       </div>
     </div>
